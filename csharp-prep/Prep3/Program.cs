afcbc5a4c11515cb1190dc29adb3429d1c78f35f
cambur01 <cambur01@gmail.com>
2024-01-20 23:38:19 -0700

    Completed week 1 prove assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.Write("What is your magic number? ");
+        // string magic = Console.ReadLine();
+        // int number = int.Parse(magic);
+
+        Random randomNumber = new Random();
+        int number = randomNumber.Next(1, 101);
+
+        int guessNum = -1;
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            string guess = Console.ReadLine();
+            guessNum = int.Parse(guess);
+
+            if (guessNum < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guessNum > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        } while (guessNum != number);
     }
 }
